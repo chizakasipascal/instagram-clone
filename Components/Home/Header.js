edff445c878333947cd8d62633f9ed034787e0d0
chizakasipascal <pascalkasichiza@gmail.com>
2022-09-12 16:56:36 +0200

    add icons style heqder

--- Components/Home/Header.js
+++ Components/Home/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity>
         <Image
           style={styles.logo}
@@ -11,16 +11,32 @@ const Header = () => {
         // source={{ uri: 'https://www.seekpng.com/png/full/813-8136306_trend-new-instagram-logo-2019-png-edigital-instagram.png' }}
         />
       </TouchableOpacity>
-
+      <View style={styles.iconsContainer}>
+        <Text style={{ color: 'white' }}>Hello</Text>
+        <Text style={{ color: 'white' }}>Hello</Text>
+        <Text style={{ color: 'white' }}>Hello</Text>
+      </View >
     </View>
   )
 }
 const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    paddingTop:20 
+  },
   logo: {
     width: 100,
     height: 50,
-    resizeMode: 'contain'
-
-  }
+    resizeMode: 'contain' 
+  },
+  icon: {
+    width:30 
+  },
+  iconsContainer: { 
+    flexDirection: 'row', 
+  } 
 })
 export default Header
